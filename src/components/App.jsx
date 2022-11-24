e98905d425c4c91154c1b59699323defb020c09c
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-11-24 01:14:09 +0100

    Bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
